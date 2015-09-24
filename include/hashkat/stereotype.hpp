35a73cef7ef67629af77211bd501116fa990b92d
Armin Sobhani <arminms@gmail.com>
2015-09-24 16:41:12 -0400

    #10: stereotype.hpp file was added

---
+++ include/hashkat/stereotype.hpp
@@ -0,0 +1,43 @@
+////////////////////////////////////////////////////////////////////////////////
+// This file is part of the #KAT Social Network Simulator.
+//
+// The #KAT Social Network Simulator is free software: you can redistribute it
+// and/or modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// The #KAT Social Network Simulator is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// the #KAT Social Network Simulator. If not, see http://www.gnu.org/licenses.
+//
+// Addendum:
+//
+// Under this license, derivations of the #KAT Social Network Simulator
+// typically must be provided in source form. The #KAT Social Network Simulator
+// and derivations thereof may be relicensed by decision of the original
+// authors (Kevin Ryczko & Adam Domurad, Isaac Tamblyn), as well, in the case
+// of a derivation, subsequent authors.
+//
+
+#ifndef HASHKAT_STEREOTYPE_HPP_
+#define HASHKAT_STEREOTYPE_HPP_
+
+namespace hashkat {
+
+template
+<
+    typename T = std::uint32_t
+,   typename V = double
+>
+class stereotype
+{
+    std::string name_;
+}
+
+}    // namespace hashkat
+
+#endif  // HASHKAT_STEREOTYPE_HPP_
