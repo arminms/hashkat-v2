e90373e54f7d826a7f8d2b0afef3ab65d6199ffb
Armin Sobhani <arminms@gmail.com>
2015-11-30 13:09:11 -0500

    unused follow_rate_ member variable was removed from twitter_follow_st class

--- include/hashkat/actions/twitter_follow_st.hpp
+++ include/hashkat/actions/twitter_follow_st.hpp
@@ -116,10 +116,6 @@ private:
     {
         base_type::rate_ = 0;
         base_type::weight_ = 0;
-        follow_rate_ = cnf_ptr_->template
-            get<typename base_type::rate_type>("agents.rates.follow", 0.0001);
-        //base_type::weight_ = cnf_ptr_->template
-        //    get<typename base_type::weight_type>("agents.rates.follow", 0.0001);
         n_connections_ = 0;
     }
 
@@ -848,7 +844,6 @@ private:
     ConfigType* cnf_ptr_;
     RngType* rng_ptr_;
     const TimeType* time_ptr_;
-    typename base_type::rate_type follow_rate_;
     std::size_t n_connections_;
     std::size_t kmax_;
     std::vector<std::unordered_set<T>> bins_;
